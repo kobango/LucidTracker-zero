d318e66b8d53c99891909b18f85f836e1fd2ec2b
grobocop12 <34235543+grobocop12@users.noreply.github.com>
2018-04-02 00:41:16 +0200

    wrzucam

---
+++ WebApplication2/Database/LucidTrackerDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using WebApplication2.Models;
+using WebApplication2;
+using System.Data.Entity;
+
+
+namespace WebApplication2.Database
+{
+    public class LucidTrackerDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        
+
+        public LucidTrackerDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+
+        }
+
+        public static LucidTrackerDbContext Create()
+        {
+            return new LucidTrackerDbContext();
+        }
+    }
+
+    public class NotesViewModelDBC : LucidTrackerDbContext
+    {
+        public DbSet<NotesViewModel> NotesViewModels { get; set; }
+    }
+    
+
+}
--- WebApplication2/Views/NotesViewModel.cs
+++ WebApplication2/Views/NotesViewModel.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-using WebApplication2.Models;
-
 
 namespace WebApplication2.Models
 {
@@ -13,5 +11,10 @@ namespace WebApplication2.Models
         public int Id { get; set; }
         public string Thema { get; set; }
         public string Text { get; set; }
+    }
+
+    public class NotesViewModelDBC : ApplicationDbContext
+    {
+        public DbSet<NotesViewModel> NotesViewModels { get; set; }
     }
 }
