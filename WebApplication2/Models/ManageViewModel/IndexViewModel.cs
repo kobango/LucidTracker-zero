8d0f149036b5a8617a9c3d20476d0daf051be904
Unknown <szeliga_d@o2.pl>
2018-04-12 00:29:43 +0200

    remove manage model, view, controller

--- WebApplication2/Models/ManageViewModel/IndexViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication2.Models.ManageViewModel
-{
-    public class IndexViewModel
-    {
-        public bool HasPassword { get; set; }
-        public bool BrowserRemembered { get; set; }
-    }
-}
