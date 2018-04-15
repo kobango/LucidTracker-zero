d0a31238ed389aad613fbb555558c001c27bad30
Unknown <szeliga_d@o2.pl>
2018-04-15 13:54:44 +0200

    fix tutorials

--- WebApplication2/Controllers/TutorialsController.cs
+++ WebApplication2/Controllers/TutorialsController.cs
@@ -8,7 +8,7 @@ namespace WebApplication2.Controllers
 {
     public class TutorialsController : Controller
     {
-<<<<<<< HEAD
+
         // GET: Tutorials
         public ActionResult Index()
         {
@@ -20,12 +20,7 @@ namespace WebApplication2.Controllers
             {
                 return RedirectToAction("Login", "Account");
             }
-=======
-        // GET: PDFViewer
-        public ActionResult Index()
-        {
-            return View();
->>>>>>> note_views
+
         }
     }
 }
